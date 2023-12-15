8f3ed1d738accf5362ebd169ba43c12beeaa73d5
Majva <m.vahidkhoo@gmail.com>
2023-12-15 16:50:23 +0330

    feat (service) : implement toggle publish method of post service

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Services;
+
 use App\Models\Post;
+
 class PostService
 {
     public function create(array $data)
@@ -9,15 +11,23 @@ class PostService
         return Post::create($data);
     }
 
-    public function update(Post $post, array $data){
+    public function update(Post $post, array $data)
+    {
         return $post->update($data);
     }
 
-    public function delete(Post $post){
+    public function delete(Post $post)
+    {
         return $post->delete();
     }
 
-    public function restore(Post $post){
+    public function restore(Post $post)
+    {
         return $post->restore();
+    }
+
+    public function togglePublish(Post $post)
+    {
+        return $this->update($post, ['published' => !$post->published]);
     }
 }
