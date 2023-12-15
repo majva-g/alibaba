ac796f359bdd4588a5a5aa3c9956bf187b52ba62
Majva <m.vahidkhoo@gmail.com>
2023-12-15 15:44:49 +0330

    refactor (model) : determine the user relation return type in post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -16,7 +17,7 @@ class Post extends Model
         'published'
     ];
 
-    public function user(){
+    public function user() : BelongsTo{
         return $this->belongsTo(User::class);
     }
 }
