cf9d8bff2c5d41dabde0ed945468c8ce06964a35
Majva <m.vahidkhoo@gmail.com>
2023-12-15 16:27:31 +0330

    feat (service) : implement the update method of post service

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Services;
+use App\Models\Post;
 class PostService
 {
     public function create(array $data)
     {
-        return \App\Models\Post::create($data);
+        return Post::create($data);
+    }
+
+    public function update(Post $post, array $data){
+        return $post->update($data);
     }
 }
